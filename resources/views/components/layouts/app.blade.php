4e3910505fdf9375e53b54c427be4a0bbd079fda
blindmelon07 <bryandetera07@gmail.com>
2025-06-19 01:53:26 -0700

    Added Front End for Sample

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,15 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ $title ?? 'GSAC' }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles()
     </head>
     <body class="bg-slate-200 dark:bg-slate-700">
+        <livewire:partials.navbar />
         <main>
              {{ $slot }}
         </main>
-        
+        <livewire:partials.footer />
         @livewireScripts()
     </body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
+use App\Livewire\Auth\ForgotPasswordPage;
+use App\Livewire\Auth\LoginPage;
+use App\Livewire\Auth\RegisterPage;
+use App\Livewire\Auth\ResetPasswordPage;
+use App\Livewire\CancelPage;
+use App\Livewire\CartPage;
+use App\Livewire\CategoriesPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\HomePage;
+use App\Livewire\MyOrderDetailPage;
+use App\Livewire\MyOrdersPage;
+use App\Livewire\ProductDetailPage;
+use App\Livewire\ProductsPage;
+use App\Livewire\SuccessPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class)->name('home');
+Route::get('/categories', CategoriesPage::class)->name('category');
+Route::get('/products',ProductsPage::class)->name('products');
+Route::get('/cart',CartPage::class)->name('cart');
+Route::get('/products/{product}', ProductDetailPage::class);
+
+Route::get('/checkout', CheckoutPage::class)->name('checkout');
+Route::get('/my-orders', MyOrdersPage::class)->name('my-orders');
+Route::get('/my-orders/{order}', MyOrderDetailPage::class)->name('my-order-detail');
+
+
+// Auth routes
+Route::get('/login',LoginPage::class)->name('login');
+Route::get('/register',RegisterPage::class)->name('register');
+Route::get('/forgot', ForgotPasswordPage::class)->name('forgot-password');
+Route::get('/reset',ResetPasswordPage::class)->name('reset-password');
+
+
+Route::get('/success', SuccessPage::class)->name('success');
+Route::get('/cancel',CancelPage::class)->name('cancel');
